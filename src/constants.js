c504d858483b0db00f5cd4a8f9614a2b49db815a
morayyajain <morayyajain@gmail.com>
2026-03-18 12:49:20 +1100

    changed wonload links

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@
  * Centralised here so a single update propagates everywhere.
  */
 
-// Desktop app download URLs (GitHub Releases)
-export const MACOS_URL = 'https://github.com/Morayya-Jain/BrainDock/releases/latest/download/BrainDock-macOS.dmg'
-export const WINDOWS_URL = 'https://github.com/Morayya-Jain/BrainDock/releases/latest/download/BrainDock-Setup.exe'
+// Desktop app download URLs (branded redirects via Netlify → GitHub Releases)
+export const MACOS_URL = '/download/macos'
+export const WINDOWS_URL = '/download/windows'
